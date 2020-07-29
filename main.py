edd3383cf84e025fe72d54c6bffb443adc3fe4fb
finall1008 <wymxx55@outlook.com>
2020-07-29 11:23:46 +0800

    修复 /start 描述过短

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     loop.run_until_complete(init_appapi(aapi))
     loop.close()
 
-    commands = [("start", "开始"), ("getpic", "使用 id 获取 pixiv 图片预览及详情")]
+    commands = [("start", "检查运行状态"), ("getpic", "使用 id 获取 pixiv 图片预览及详情")]
     updater.bot.set_my_commands(commands)
 
     dispatcher.add_handler(CommandHandler("getpic", send_illust_info))
